46a54e6ce31fbc910d304c0458b4a5443cc52168
Max Silver <Maxbsilver@gmail.com>
2019-04-21 08:31:10 -0600

    Updates

---
+++ precache-manifest.8447a5f7a713f92b7531568e3d5d7a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52ad886bf25b35d301e7",
+    "url": "/portfolio/static/css/main.22fd203a.chunk.css"
+  },
+  {
+    "revision": "52ad886bf25b35d301e7",
+    "url": "/portfolio/static/js/main.42af17fd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d69fad560ede48fd0e91",
+    "url": "/portfolio/static/js/2.01c4378b.chunk.js"
+  },
+  {
+    "revision": "78a9e53a74c5bfd5124544d5c8fc75a4",
+    "url": "/portfolio/index.html"
+  }
+];
